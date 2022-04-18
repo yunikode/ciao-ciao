78d758482753520b98f17cdcf52acf8651e07c5c
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2022-04-18 16:22:48 +0200

    2.2 Solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	   System.out.println("<svg height=\"400\" width=\"1000\">\n  <circle cx=\"100\" cy=\"100\" r=\"50\" />\n</svg>");
+        int x = new java.util.Scanner(System.in).nextInt();
+        int y =  new java.util.Scanner(System.in).nextInt();
+        double r = 10 + Math.random()*10;
+	   System.out.println("<svg height=\"400\" width=\"1000\">\n  <circle cx=\""+x+"\" cy=\""+y+"\" r=\""+r+"\" />\n</svg>");
     }
 }
