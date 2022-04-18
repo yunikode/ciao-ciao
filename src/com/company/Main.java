cad3ccd81d616f5cd859ef20b6390235454f43e3
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2022-04-18 16:12:45 +0200

    2.1 Solved

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   System.out.println("<svg height=\"400\" width=\"1000\">\n  <circle cx=\"100\" cy=\"100\" r=\"50\" />\n</svg>");
+    }
+}
